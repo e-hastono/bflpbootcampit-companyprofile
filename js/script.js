6522a2e0bcee0989f8bf9a4487c663a6faee9329
Emmanoel Hastono <16159393+e-hastono@users.noreply.github.com>
2024-02-05 09:48:57 +0700

    Todo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+let todoList = [];
+
+const setToLocalStorage = () => {
+  localStorage.setItem('todos', JSON.stringify(todoList));
+}
+
+const deleteTodo = (dataKey) => {
+  todoList = todoList.filter(todo => todo.id != dataKey);
+  setToLocalStorage();
+
+  document.getElementById(`todo-${dataKey}`).remove();
+}
+
+const handleCheckTodo = (checkbox, dataKey, todoDate, todoTime) => {
+  const tagToChange = document.getElementById(`todo-name-${dataKey}`);
+  const statusToChange = document.getElementById(`todo-status-${dataKey}`)
+
+  if (checkbox.checked == true) {
+    tagToChange.classList.add('text-decoration-line-through');
+  } else {
+    tagToChange.classList.remove('text-decoration-line-through');
+  }
+
+  statusToChange.innerHTML = getTodoStatus(checkbox.checked == true, todoDate, todoTime)
+}
+
+const getTodoStatus = (isComplete, todoDate, todoTime) => {
+  let todoStatus = '<span class="badge bg-success">Completed<span>'
+
+  if (!isComplete) {
+    if (new Date() <= new Date(`${todoDate} ${todoTime}`)) {
+      todoStatus = '<span class="badge bg-secondary">On Progress</span>'
+    } else {
+      todoStatus = '<span class="badge bg-danger">Overdue</span>'
+    }
+  }
+
+  return todoStatus
+}
+
 (function ($) {
 
   "use strict";
@@ -116,7 +156,66 @@
       },
     });
 
-    
+    const todoForm = document.getElementById("todoForm");
+
+    const todoListGroup = document.getElementById("todo-list-group");
+
+    const renderTodo = (todo) => {
+      const li = document.createElement('li');
+      li.setAttribute('id', `todo-${todo.id}`);
+      li.setAttribute('class', 'list-group-item d-flex w-100 justify-content-between align-items-center');
+      li.setAttribute('data-key', todo.id);
+
+      // get status
+
+      const todoStatus = getTodoStatus(todo.completed, todo.date, todo.time)
+
+      li.innerHTML = `
+        <input type="checkbox" class="form-check-input" ${todo.completed ? 'checked' : ''} onchange="handleCheckTodo(this, ${todo.id}, '${todo.date}', '${todo.time}')">
+        <h4 id="todo-name-${todo.id}">${todo.name}<h4>
+        <h5>Due: ${todo.date} ${todo.time}</h5>
+        <h6 id="todo-status-${todo.id}">${todoStatus}</h6> 
+        <button class="btn btn-danger" onclick="deleteTodo(${todo.id})">X</button>
+      `;
+
+      todoListGroup.append(li);
+    }
+
+    const getFromLocalStorage = () => {
+      const localStorageTodoList = localStorage.getItem('todos');
+
+      if (localStorageTodoList) todoList = JSON.parse(localStorageTodoList);
+
+      todoList.forEach(todo => {
+        renderTodo(todo);
+      });
+    }
+
+    getFromLocalStorage();
+
+    todoForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const newTodoName = event.target[0].value;
+      const newTodoDate = event.target[1].value;
+      const newTodoTime = event.target[2].value;
+
+      const todoObj = {
+        id: Date.now(),
+        name: newTodoName,
+        date: newTodoDate,
+        time: newTodoTime,
+        completed: false,
+      };
+
+      todoList.push(todoObj);
+
+      localStorage.setItem('todos', JSON.stringify(todoList));
+
+      renderTodo(todoObj)
+
+      todoForm.reset()
+    });
 
   }); // End of a document ready
 
